3d66346c6430233943a00ea61635d5a94ec7b5f2
Goshya <tormozov.g@mail.ru>
2024-09-18 17:42:16 +0300

    main method update

--- SwitchStatementAnalyzer/Program.cs
+++ SwitchStatementAnalyzer/Program.cs
@@ -18,9 +18,46 @@ namespace SwitchStatementAnalyzer
 	{
 		static async Task Main(string[] args)
 		{
-			
-		}
+			if (args.Length != 2) 
+			{
+				Console.WriteLine("Invalid input, type -h for help");
+				return;
+			}
+			// Creating workspace
+			MSBuildLocator.RegisterDefaults();
+			MSBuildWorkspace workspace = MSBuildWorkspace.Create();
 
+			// Openning solution
+			Solution solution = workspace.OpenSolutionAsync(args[0]).Result;
+			IEnumerable<Project> projects = solution.Projects;
+			Project currProject = projects.First();
+
+			Compilation compilation = currProject.GetCompilationAsync().Result;
+
+			foreach (var file in currProject.Documents) 
+			{
+				Console.WriteLine($"Analyzing file: {file.Name}");
+				SyntaxTree tree = file.GetSyntaxTreeAsync().Result;
+				SemanticModel semanticModel = compilation.GetSemanticModel(tree);
+
+				IEnumerable<SwitchStatementSyntax> switchStatementSyntaxes = tree.GetRoot().DescendantNodes().OfType<SwitchStatementSyntax>();
+
+				foreach(SwitchStatementSyntax switchStatement in switchStatementSyntaxes)
+				{
+					IdentifierNameSyntax identifierName = switchStatement.DescendantNodes().OfType<IdentifierNameSyntax>().First();
+					ITypeSymbol identifierType = semanticModel.GetTypeInfo(identifierName).Type;
+
+					if (identifierType.TypeKind == TypeKind.Enum) 
+					{
+						Console.WriteLine($"Switch по перечислению, количество членов в перечислении: {identifierType.GetMembers().Length - 1}");
+					}
+
+					IEnumerable<DefaultSwitchLabelSyntax> defaultSwitch = switchStatement.DescendantNodes().OfType<DefaultSwitchLabelSyntax>();
+					if (defaultSwitch.Count() > 0) { Console.WriteLine("Default case detected"); }
+				}
+			}
+
+		}
 		
 
 	}
